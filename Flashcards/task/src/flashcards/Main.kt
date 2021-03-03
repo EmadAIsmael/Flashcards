5c7bf68a24def8d3a7a834499d54f506695c295f
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-03-03 13:34:59 +0200

    Wed., Mar. 3rd. 2021 13:34 finished stage 3/7.

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -3,19 +3,46 @@ package flashcards
 import java.util.Scanner
 
 fun main() {
-    println("Input (a term, then a definition, and, finally, an answer):\n")
-    val card = input()
-    val definition = input()
-    val answer = input()
-
-    if (definition == answer)
-        println("Your answer is right!")
-    else
-        println("Your nswer is wrong...")
+    val flashCards = FlashCards()
+    flashCards.getCards()
+    flashCards.allCardsQuiz()
 }
-fun input(prompt: String = ""): String? {
+fun input(prompt: String = ""): String {
     val scanner = Scanner(System.`in`)
 
     print(prompt)
     return scanner.nextLine()
+}
+
+class FlashCards {
+    data class Card(val term: String, val definition: String)
+    private var numberOfCards = 0
+    private val cards = mutableListOf<Card>()
+
+    private fun getOneCard(cardNumber: Int) {
+        val term = input("Card #${cardNumber + 1}:\n")
+        val definition = input("The definition for card #${cardNumber + 1}:\n")
+        cards.add(Card(term, definition))
+    }
+
+    fun getCards() {
+        numberOfCards = input("Input the number of cards:\n").toInt()
+
+        repeat(numberOfCards) {
+            getOneCard(it)
+        }
+    }
+
+    private fun oneCardQuiz(card: Card) {
+        val answer = input("Print the definition of \"${card.term}\":\n")
+        if (answer == card.definition)
+            println("Correct!")
+        else
+            println("Wrong. The right answer is \"${card.definition}\".")
+    }
+
+    fun allCardsQuiz() {
+        for (card in cards)
+            oneCardQuiz(card)
+    }
 }

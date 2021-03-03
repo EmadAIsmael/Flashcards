ce1c046996b005889f19a6dcb8b1a34e2736ad11
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-03-03 21:05:22 +0200

    Wed., Mar. 3rd. 2021 21:05 finished stage 4/7.

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -2,11 +2,6 @@ package flashcards
 
 import java.util.Scanner
 
-fun main() {
-    val flashCards = FlashCards()
-    flashCards.getCards()
-    flashCards.allCardsQuiz()
-}
 fun input(prompt: String = ""): String {
     val scanner = Scanner(System.`in`)
 
@@ -15,14 +10,55 @@ fun input(prompt: String = ""): String {
 }
 
 class FlashCards {
-    data class Card(val term: String, val definition: String)
     private var numberOfCards = 0
+    private val data = mutableMapOf<Int, String>()
+    private val termToDef = mutableMapOf<Int, Int>()
+    private val defToTerm = mutableMapOf<Int, Int>()
+
+    data class Card(val term: Int, val definition: Int)
+
     private val cards = mutableListOf<Card>()
 
+    private fun readTerm(cardNumber: Int): String {
+        var term = ""
+        var isRepeatedTerm = false
+        println("Card #${cardNumber + 1}:")
+        while (!isRepeatedTerm) {
+            term = input()
+            if (data.getOrDefault(term.hashCode(), "") == "") {
+                isRepeatedTerm = true
+            } else
+                println("The term \"${term}\" already exists. Try again: ")
+        }
+        return term
+    }
+
+    private fun readDefinition(cardNumber: Int): String {
+        var definition = ""
+        var isRepeatedTerm = false
+        println("The definition for card #${cardNumber + 1}:")
+        while (!isRepeatedTerm) {
+            definition = input()
+            if (data.getOrDefault(definition.hashCode(), "") == "") {
+                isRepeatedTerm = true
+            } else
+                println("The definition \"${definition}\" already exists. Try again:")
+        }
+        return definition
+    }
+
     private fun getOneCard(cardNumber: Int) {
-        val term = input("Card #${cardNumber + 1}:\n")
-        val definition = input("The definition for card #${cardNumber + 1}:\n")
-        cards.add(Card(term, definition))
+        val theTerm = readTerm(cardNumber)
+        val theDefinition = readDefinition(cardNumber)
+
+        val termHashCode = theTerm.hashCode()
+        val defHashCode = theDefinition.hashCode()
+        data[termHashCode] = theTerm
+        data[defHashCode] = theDefinition
+        termToDef[termHashCode] = defHashCode
+        defToTerm[defHashCode] = termHashCode
+
+         cards.add(Card(termHashCode, defHashCode))
     }
 
     fun getCards() {
@@ -33,16 +69,43 @@ class FlashCards {
         }
     }
 
+    fun getDefByTerm(term: Int): Int? {
+        return termToDef[term]
+    }
+
+    fun getTermByDef(def: Int): Int? {
+        return defToTerm[def]
+    }
+
     private fun oneCardQuiz(card: Card) {
-        val answer = input("Print the definition of \"${card.term}\":\n")
-        if (answer == card.definition)
-            println("Correct!")
-        else
-            println("Wrong. The right answer is \"${card.definition}\".")
+
+        val termText = data[card.term]
+        val defText = data[card.definition]
+        val answer = input("Print the definition of \"${termText}\":\n")
+        val userDefHashCode = answer.hashCode()
+
+        when {
+            userDefHashCode == getDefByTerm(card.term) -> println("Correct!")
+            getTermByDef(userDefHashCode)  != null -> {
+                val correctTerm = data[defToTerm[answer.hashCode()]]
+                println(
+                    "Wrong. The right answer is \"${defText}\", " +
+                            "but your definition is correct for " +
+                            "\"${correctTerm}\"."
+                )
+            }
+            else -> println("Wrong. The right answer is \"${data[card.definition]}\".")
+        }
     }
 
     fun allCardsQuiz() {
         for (card in cards)
             oneCardQuiz(card)
     }
+}
+
+fun main() {
+    val flashCards = FlashCards()
+    flashCards.getCards()
+    flashCards.allCardsQuiz()
 }
